4752db7b74c9f91d808bd339f4504dd951bfb8b3
DrabornEagle <draborneagle@gmail.com>
2026-05-31 00:10:31 +0300

    Add Anyela Born Miami packages payment page

---
+++ AnyelaBorn/Packages/assets/dkd_anyela_config.js
@@ -0,0 +1,6 @@
+window.dkd_anyela_payment_config = {
+  dkd_webhook_url: "",
+  dkd_payment_reference_prefix: "ANYELA",
+  dkd_bank_iban: "TR53 0011 1000 0000 0151 1527 14",
+  dkd_account_holder: "Doğancan Kartal"
+};
