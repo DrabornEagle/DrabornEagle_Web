631ce45c208d09171cadd0729b70a88da0d544af
DrabornEagle <draborneagle@gmail.com>
2026-06-12 10:29:29 +0300

    Update admin server wrapper visible version to v0.34

--- deal-admin/src/dkd_server_v0_23.js
+++ deal-admin/src/dkd_server_v0_23.js
@@ -5,8 +5,10 @@ console.log = (...dkdArgs) => {
   const dkdPatchedArgs = dkdArgs.map((dkdArg) => {
     if (typeof dkdArg !== 'string') return dkdArg;
     return dkdArg
-      .replace(/"dkd_version":"v0\.17\.1"/g, '"dkd_version":"v0.23"')
-      .replace(/"dkd_stats_version":"v0\.17\.1"/g, '"dkd_stats_version":"v0.23"');
+      .replace(/"dkd_version":"v0\.17\.1"/g, '"dkd_version":"v0.34"')
+      .replace(/"dkd_version":"v0\.23"/g, '"dkd_version":"v0.34"')
+      .replace(/"dkd_stats_version":"v0\.17\.1"/g, '"dkd_stats_version":"v0.34"')
+      .replace(/"dkd_stats_version":"v0\.23"/g, '"dkd_stats_version":"v0.34"');
   });
   dkdOriginalConsoleLog(...dkdPatchedArgs);
 };
