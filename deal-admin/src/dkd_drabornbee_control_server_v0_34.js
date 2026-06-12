cb8a10a59a00d8051f7416cf05d99ff51d3c3305
DrabornEagle <draborneagle@gmail.com>
2026-06-12 10:30:47 +0300

    Patch DraBornBee control logs to v0.34

--- deal-admin/src/dkd_drabornbee_control_server_v0_34.js
+++ deal-admin/src/dkd_drabornbee_control_server_v0_34.js
@@ -1,2 +1,7 @@
+const dkdOriginalLog = console.log.bind(console);
+console.log = (...dkdArgs) => {
+  const dkdNextArgs = dkdArgs.map((dkdArg) => typeof dkdArg === 'string' ? dkdArg.replace(/"dkd_version":"v0\.30"/g, '"dkd_version":"v0.34"').replace(/worker_started version=v0\.30/g, 'DraBornBee started version=v0.34') : dkdArg);
+  dkdOriginalLog(...dkdNextArgs);
+};
 console.log(JSON.stringify({ dkd_worker_engine: 'DraBornBee', dkd_version: 'v0.34' }));
 await import('./dkd_worker_control_server_v0_30.js');
