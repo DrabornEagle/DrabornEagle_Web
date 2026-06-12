2e7658bd34efae3f0248fa5bbfeb9bbb3b56b077
DrabornEagle <draborneagle@gmail.com>
2026-06-12 04:36:51 +0300

    Add visible v0.23 admin server wrapper

---
+++ deal-admin/src/dkd_server_v0_23.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+const dkdOriginalConsoleLog = console.log.bind(console);
+console.log = (...dkdArgs) => {
+  const dkdPatchedArgs = dkdArgs.map((dkdArg) => {
+    if (typeof dkdArg !== 'string') return dkdArg;
+    return dkdArg
+      .replace(/"dkd_version":"v0\.17\.1"/g, '"dkd_version":"v0.23"')
+      .replace(/"dkd_stats_version":"v0\.17\.1"/g, '"dkd_stats_version":"v0.23"');
+  });
+  dkdOriginalConsoleLog(...dkdPatchedArgs);
+};
+
+await import('./dkd_server_v0_17_1.js');
