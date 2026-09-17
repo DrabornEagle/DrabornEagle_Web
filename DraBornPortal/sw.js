beeb812b4ab866bf0f5cedaaf525c8cd4c0c6dd6
DrabornEagle <draborneagle@gmail.com>
2026-09-18 02:04:38 +0300

    chore: refresh DraBornPortal v0.3.2 cache

--- DraBornPortal/sw.js
+++ DraBornPortal/sw.js
@@ -1,5 +1,5 @@
-const CACHE='drabornportal-v0.3.1-20260918';
-const ASSETS=['./','./index.html','./styles.css?v=20260918-v031','./app.js?v=20260918-v031','./manifest.webmanifest'];
+const CACHE='drabornportal-v0.3.2-20260918';
+const ASSETS=['./','./index.html','./styles.css?v=20260918-v032','./app.js?v=20260918-v032','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
